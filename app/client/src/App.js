33fd2f695871abc06f075497133170fd516f28db
akashsonune <akashsonune7720@gmail.com>
2024-03-02 23:47:08 +0530

    updates

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import{Route,Routes} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Counter from './components/02-03/Counter';
 
 function App() {
   return (
     <div className="App">
-      wellcome
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/Login' element={<Login/>} />
+    <Route path='/Register' element={<Register/>} />
+    <Route path='/counter' element={<Counter/>} />
+    </Routes>
       
     </div>
   );
---
+++ app/client/src/components/02-03/Counter.jsx
@@ -0,0 +1,39 @@
+
+
+import { useState } from "react"
+
+
+function Counter(){
+    const initalPrice=1000
+    const [Counter ,setCounter] = useState(0)
+    const [price,setPrice] = useState(initalPrice);
+
+    function Increment(){
+        if(Counter <10 ){
+            setCounter(prevValue => prevValue+1)
+            setPrice(initalPrice *(Counter + 1))
+        }
+    }
+
+    function Decrement(){
+             if(Counter > 0){
+               setCounter(prevValue => prevValue -1)
+               setPrice(initalPrice *(Counter - 1))
+             }
+    }
+
+    function Reset(){
+           setPrice(0)
+        setCounter(0)
+    }
+    return(
+        <div>
+            <h1>Counter-{Counter} and price-{price}</h1>
+            <button onClick={Increment}>+</button>
+            <button onClick={Decrement}>-</button>
+            <button onClick={Reset}>Reset</button>
+
+        </div>
+    )
+}
+export default Counter;
