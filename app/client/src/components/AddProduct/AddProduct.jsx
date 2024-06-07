4c4bf91fa11fc9e105e737433d7e09527f6d1da9
akashsonune <akashsonune7720@gmail.com>
2024-06-07 13:07:28 +0530

    updates

--- app/client/src/components/AddProduct/AddProduct.jsx
+++ app/client/src/components/AddProduct/AddProduct.jsx
@@ -7,63 +7,6 @@ import api from '../../AxiosConfig';
 import SellerProtected from '../redireactions/SellerProtected';
 
 const AddProduct = () => {
-//   const [productData, setProductData] = useState({
-//     name: "",
-//     category: "",
-//     price: "",
-//     quantity: "",
-//     tags: ""
-//   });
-
-//   console.log(productData, "productData");
-
-//   const handleChange = (event) => {
-//     setProductData({ ...productData, [event.target.name]: event.target.value });
-//   };
-
-//   const { state } = useContext(AuthContext);
-//   console.log(state, "state");
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//     try {
-//       const response = await api.post("/api/v1/product/add-product", {
-//         productData,
-//         userId: state.user._id,
-//       });
-
-//       if (response.data.success) {
-//         toast.success(response.data.message);
-//       }
-//     } catch (error) {
-//       toast.error(error.response.data.error);
-//     }
-//   };
-
-//   return (
-//    <SellerProtected>
-//       <form onSubmit={handleSubmit}>
-//         <h1>Add Product</h1>
-//         <label>Product name</label><br/>
-//         <input type="text" required name="name" onChange={handleChange} /><br/>
-
-//         <label>Product category</label><br/>
-//         <input type="text" required name="category" onChange={handleChange} /><br/>
-
-//         <label>Product price</label><br/>
-//         <input type="text" required name="price" onChange={handleChange} /><br/>
-
-//         <label>Product quantity</label><br/>
-//         <input type="text" required name="quantity" onChange={handleChange} /><br/>
-
-//         <label>Product tags</label><br/>
-//         <input type="text" required name="tags" onChange={handleChange} /><br/>
-
-//         <input type='submit' /><br/>
-//       </form>
-//       </SellerProtected>
-//   );
-
 const [productData, setProductData] = useState({
   name: "",
   category: "",
@@ -92,6 +35,7 @@ const handleSubmit = async (event) => {
       toast.success(response.data.message);
     }
   } catch (error) {
+    console.log(error);
     toast.error(error.response.data.error);
   }
 };
--- app/client/src/components/AllProducts.jsx
+++ app/client/src/components/AllProducts.jsx
@@ -1,71 +1,4 @@
 
-
-
-
-// // import axios from 'axios';
-// import React, { useEffect, useState } from 'react'
-// import api from '../AxiosConfig';
-
-// const AllProducts = (props)=> {
-//   const {awdiz} = props;
-//   console.log(awdiz);
-
-//   const [allProducts ,setAllProducts] =useState([]);
-
- 
-//   useEffect(()=>{
-//    async function getProducts(){
-//       try {
-//         const response = await api.get("/api/v1/product/get-products")
-//         if(response?.data?.success){
-//           // console.log(response.data.products);
-//           setAllProducts(response.data.products);
-
-//         }
-        
-//       } catch (error) {
-//         console.log(error);
-//       }
-
-//     }
-//     getProducts();
-    
-//   },[])
-
-
-
-//   return (
-//     <div>
-//          <h1>AllProducts</h1>
-//           {allProducts.length? 
-          
-//           <div style={{display:"flex" ,flexWrap:"wrap" , justifyContent :"space-around"}}>
-//            {allProducts.map((productObj)=>(
-//            <div style={{width :"15%" , border:"2px solid black" , marginBottom:"5%", height:"250px"}}> 
-//                {/* <img style={{height:"54%" , width:"100%"}} src={productObj.image} /> */}
-//                <h1>Name :- {productObj.name}</h1>
-//               <p>Category :- {productObj.category}</p>
-//               <p>Price :- {productObj.price}/-</p>
-//               <p>Total Quantities :- {productObj.quantity}</p>
-//               <p>Tags :- {productObj.tags}</p>
-
-//            </div>
-//          ))}
-//         </div>:
-
-//          <div><h1>Loding..</h1></div>}
-       
-//     </div>
-//   )
-// }
-
-// export default AllProducts
-
-
-
-
-
-// import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import api from '../AxiosConfig';
 
@@ -115,6 +48,7 @@ if (response?.data?.success){
                   }}
                 >
                   <h1>Name : {productObj.name}</h1>
+                  <p>image:-{productObj.image}</p>
                   <p>Category : {productObj.category}</p>
                   <p>Price : {productObj.price}/-</p>
                   <p>Total Quantities : {productObj.quantity}</p>
