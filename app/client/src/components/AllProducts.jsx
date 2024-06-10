9824027df3839bb4a74a72db7843c43f818db32e
akashsonune <akashsonune7720@gmail.com>
2024-06-10 21:42:32 +0530

    updates

--- app/client/src/components/AllProducts.jsx
+++ app/client/src/components/AllProducts.jsx
@@ -1,72 +1,60 @@
 
-import React, { useEffect, useState } from 'react'
-import api from '../AxiosConfig';
-
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import api from "../../AxiosConfig";
 
 const AllProducts = (props) => {
-    const { awdiz } = props;
-    console.log(awdiz, "awdiz");
-const [allProducts,setAllProducts] = useState([]);
+  const { awdiz } = props;
+  console.log(awdiz, "awdiz");
+  const [allProducts, setAllProducts] = useState([]);
 
-
-    useEffect(()=>{
-       async function getProducts(){
-            try{
-const response = await api.get("/api/v1/product/get-products");
-
-// const response = await api.get("/get-products");
-
-if (response?.data?.success){
-    setAllProducts(response.data.products);
-}
-            }catch (error){
-                console.log(error);
-            }
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await api.get("/api/v1/product/get-products");
+        if (response?.data?.success) {
+          setAllProducts(response.data.products);
         }
-        getProducts();
-    },[]);
-
-
-    return (
-        <div>
-          <h1>All Products</h1>
-          {allProducts.length ? (
-           <div
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <h1>All Products</h1>
+      {allProducts.length ? (
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          {allProducts.map((productObj) => (
+            <div
               style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-around",
+                width: "18%",
+                border: "2px solid black",
+                height: "250px",
               }}
             >
-              {allProducts.map((productObj) => (
-                <div
-                  style={{
-                    width: "18%",
-                    border: "2px solid black",
-                    height: "250px",
-                  }}
-                >
-                  <h1>Name : {productObj.name}</h1>
-                  {/* <p>image:-{productObj.image}</p> */}
-                  <p>Category : {productObj.category}</p>
-                  <p>Price : {productObj.price}/-</p>
-                  <p>Total Quantities : {productObj.quantity}</p>
-                  <p>{productObj.tags}</p>
-                </div>
-              ))}
+              <h1>Name : {productObj.name}</h1>
+              <p>Category : {productObj.category}</p>
+              <p>Price : {productObj.price}/-</p>
+              <p>Total Quantities : {productObj.quantity}</p>
+              <p>{productObj.tags}</p>
             </div>
-          ) : (
-            <div>
-              <h1>Loading..</h1>
-            </div>
-          )} 
+          ))}
         </div>
-      );
-    };
-    
-    export default AllProducts;
+      ) : (
+        <div>
+          <h1>Loading..</h1>
+        </div>
+      )}
+    </div>
+  );
+};
 
-
-
-
+export default AllProducts;
